7a8420759391c5519e98e1e26053c75a77f35e64
tsiresymila <tsiresymila@gmail.com>
2024-04-30 18:36:24 +0300

    [release] 0.3.0

--- src/nestipy_cli/templates/project/app_controller.py
+++ src/nestipy_cli/templates/project/app_controller.py
@@ -1,5 +1,5 @@
 from nestipy.common import Controller, Get, Post, Put, Delete
-from nestipy_ioc import Inject, Body, Params
+from nestipy.ioc import Inject, Body, Params
 
 from app_service import AppService
 
