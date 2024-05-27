49de41a2986be2c00a27634fe8042d76f9e4003b
tsiresymila <tsiresymila@gmail.com>
2024-05-27 16:21:46 +0300

    [release] 0.3.2

--- src/nestipy_cli/templates/project/app_controller.py
+++ src/nestipy_cli/templates/project/app_controller.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from nestipy.common import Controller, Get, Post, Put, Delete
-from nestipy.ioc import Inject, Body, Params
+from nestipy.ioc import Inject, Body, Param
 
 from app_service import AppService
 
@@ -19,9 +19,9 @@ class AppController:
         return await self.service.post(data=data)
 
     @Put('/{app_id}')
-    async def put(self, app_id: Annotated[int, Params('app_id')], data: Annotated[dict, Body()]) -> str:
+    async def put(self, app_id: Annotated[int, Param('app_id')], data: Annotated[dict, Body()]) -> str:
         return await self.service.put(id_=app_id, data=data)
 
     @Delete('/{app_id}')
-    async def delete(self, app_id: Annotated[int, Params('app_id')]) -> None:
+    async def delete(self, app_id: Annotated[int, Param('app_id')]) -> None:
         await self.service.delete(id_=app_id)
