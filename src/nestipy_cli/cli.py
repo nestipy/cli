61565bf8f81603d79dc9d7904445d60759eebc69
Tsiresy Milà <tsiresymila@gmail.com>
2025-07-28 21:22:27 +0300

    update(echo): update echo

--- src/nestipy_cli/cli.py
+++ src/nestipy_cli/cli.py
@@ -46,7 +46,7 @@ def new(name):
         echo.error(f"Folder {name} already exist.")
         return
     echo.info(f"Project {name} created successfully.\nStart your project by running:\n\tcd {name}"
-              f"\n\tpython -m pip install -r requirements.py\n\tnestipy start --dev")
+              f"\n\tuv sync \n\tnestipy start --dev")
     # else:
     #     echo.error(f"Nestipy need poetry as dependency manager.")
 
